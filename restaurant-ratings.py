2de6b6d8c26a78d8a18ac773cbd62c6de7de3faf
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:07:00 -0800

    Finished part 1

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,23 @@
 # your code goes here
+""" Reads the ratings in from the file
+Stores them in a dictionary, and then
+Spits out the ratings in alphabetical order by restaurant """
+
+def display_ratings(filepath):
+    """Takes a file with multiple lines and spits out names and ratings of restaurants in alphabetical order."""
+
+    open_file = open(filepath)
+
+    restaurants_and_ratings = {}
+
+    for line in open_file:
+        restaurant, rating = line.split(":")
+        restaurants_and_ratings[restaurant] = rating
+
+    alphabetized_keys = sorted(restaurants_and_ratings.keys())
+
+    for key in alphabetized_keys:
+        print key, "is rated", restaurants_and_ratings[key]
+
+
+display_ratings("scores.txt")
