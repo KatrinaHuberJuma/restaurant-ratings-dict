065779b6559053d4be971b5cba009b25597c0b25
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:26:42 -0800

    Program accepts a new restaurant and rating and adds to dict.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,7 +1,8 @@
 # your code goes here
 """ Reads the ratings in from the file
 Stores them in a dictionary, and then
-Spits out the ratings in alphabetical order by restaurant """
+Spits out the ratings in alphabetical order by restaurant
+"""
 
 def display_ratings(filepath):
     """Takes a file with multiple lines and spits out names and ratings of restaurants in alphabetical order."""
@@ -10,7 +11,10 @@ def display_ratings(filepath):
 
     restaurants_and_ratings = {}
 
+    adds_new_restaurant(restaurants_and_ratings)
+
     for line in open_file:
+        line = line.rstrip()
         restaurant, rating = line.split(":")
         restaurants_and_ratings[restaurant] = rating
 
@@ -19,5 +23,13 @@ def display_ratings(filepath):
     for key in alphabetized_keys:
         print key, "is rated", restaurants_and_ratings[key]
 
+    open_file.close()
+
+
+
+def adds_new_restaurant(established_restaurants):
+    user_added_restaurant = raw_input("Enter a new restaurant ")
+    user_added_rating = int(raw_input("Enter that restaurant's rating "))
+    established_restaurants[user_added_restaurant] = user_added_rating
 
 display_ratings("scores.txt")
