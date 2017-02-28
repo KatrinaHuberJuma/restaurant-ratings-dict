e6336a76766e06de7d9f7f17e447fc9c3447cf67
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:46:56 -0800

    added random restaurant rating updater

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,7 +2,15 @@
 """ Reads the ratings in from the file
 Stores them in a dictionary, and then
 Spits out the ratings in alphabetical order by restaurant
+
+Choose a random restaurant from the list in the file (hint: the choice() function in the Random module will probably come in handy here)
+Tell the user the chosen restaurant and its rating
+Ask the user what the new rating should be
+Update the rating
+
 """
+import random
+
 
 def display_ratings(filepath):
     """Takes a file with multiple lines and spits out names and ratings of restaurants in alphabetical order."""
@@ -19,6 +27,10 @@ def display_ratings(filepath):
         restaurants_and_ratings[restaurant] = rating
 
     alphabetized_keys = sorted(restaurants_and_ratings.keys())
+
+    random_restaurant_name = random.choice(alphabetized_keys)
+
+    restaurants_and_ratings[random_restaurant_name] = raw_input("the rating of %s is %s, what should the new rating be? " % (random_restaurant_name, restaurants_and_ratings[random_restaurant_name]))
 
     for key in alphabetized_keys:
         print key, "is rated", restaurants_and_ratings[key]
